41bba713fa169a0735c15e7a2e96ca79c0b3a6a4
Limespy <37109901+Limespy@users.noreply.github.com>
2025-02-24 10:15:39 +0200

    v0.0.1
    Initialised

--- src/numba_linalg/__init__.py
+++ src/numba_linalg/__init__.py
@@ -1,5 +1,10 @@
-"""Documentation."""
+"""Numba Linalg.
+
+Linear algebra package using Numba and compatible with Numba
+"""
+
 __version__ = '0.0.1'
+
 from importlib import import_module
 from typing import TYPE_CHECKING
 from sys import modules as _modules
@@ -10,13 +15,13 @@ from ._API import *
 if TYPE_CHECKING:
     from types import ModuleType
 
-    from . import debug
+    # from . import subpackage
 else:
     ModuleType = object
 # ======================================================================
 def __getattr__(name: str) -> ModuleType:
-    if name not in ('debug', ):
-        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
-    module = import_module(f'.{name}', __package__)
-    setattr(_modules[__package__], name, module)
-    return module
+    if name in {'subpackage', }:
+        module = import_module(f'.{name}', __package__)
+        setattr(_modules[__package__], name, module)
+        return module
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
